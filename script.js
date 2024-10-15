c6a06674a0881c1b04f181634e1efa80d3794449
Rafael Santos <rafa.05125@gmail.com>
2024-10-15 18:48:02 -0300

    Project finished

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/010021000/json/")
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) {
-      throw Error("Esse cep não existe!");
-    } else console.log(r);
-  })
-  .catch((erro) => console.log(erro))
-  .finally((mensagem) => console.log("Processamento concluido!"));
+async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
+  try {
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertido = await consultaCep.json();
+    if (consultaCEPConvertido.erro) {
+      throw Error("CEP não existente!");
+    }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
 
-console.log(consultaCEP);
+    cidade.value = consultaCEPConvertido.localidade;
+    logradouro.value = consultaCEPConvertido.logradouro;
+    estado.value = consultaCEPConvertido.uf;
+    console.log(consultaCEPConvertido);
+    return consultaCEPConvertido;
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+    console.log(erro);
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
